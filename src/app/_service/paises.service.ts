a9f1a33c32672a5e811d62a955661010cbf2cd02
edwarmoran <edwar.moran.12@gmail.com>
2021-02-12 19:32:58 -0500

    add service combobox

---
+++ src/app/_service/paises.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisesService {
+
+  url : string ='https://coronavirus-tracker-api.herokuapp.com/v2/locations/';
+  
+  constructor(
+    private http : HttpClient
+  ) { }
+
+  listar(){
+    console.log(this.url);
+    return this.http.get<any>(this.url);
+    
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { PaisesService } from './_service/paises.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-rest-covid';
+
+  localidad = [];
+  constructor(
+    private paisesService: PaisesService
+  ) { }
+
+  ngOnInit() {
+    this.paisesService.listar().subscribe(data => {
+      this.localidad = data.locations;
+    });
+  };
 }
